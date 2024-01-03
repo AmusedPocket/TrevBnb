7bee11e429fd563f43d068c75d9d921b24b4e947
Trevor <trevirv@gmail.com>
2024-01-03 03:41:48 -0800

    on delete cascades added for fk's

--- backend/db/migrations/4-create-bookings.js
+++ backend/db/migrations/4-create-bookings.js
@@ -19,7 +19,8 @@ module.exports = {
         references: {
           model: 'Spots',
           key: 'id'
-        }
+        },
+        onDelete: 'CASCADE'
       },
       userId: {
         type: Sequelize.INTEGER,
@@ -27,7 +28,8 @@ module.exports = {
         references: {
           model: 'Users',
           key: 'id'
-        }
+        },
+        onDelete: 'CASCADE'
       },
       startDate: {
         type: Sequelize.DATEONLY,
