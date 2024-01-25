3694ef3843f5bfff3f0b163a83291db6379d1588
Trevor <trevirv@gmail.com>
2024-01-25 09:41:14 -0800

    seeders updated

--- backend/db/seeders/20231222212747-SpotSeeder.js
+++ backend/db/seeders/20231222212747-SpotSeeder.js
@@ -11,147 +11,147 @@ if (process.env.NODE_ENV === 'production') {
 const validSpots = [
   {
     ownerId: 1,
-    address: "123 Jinglebell Lane",
-    city: "North Pole",
-    state: "AK",
+    address: "1509 Union Street",
+    city: "Alameda",
+    state: "CA",
     country: "USA",
-    lat: 123.456, 
-    lng: 456.789,
-    name: "North Pole",
-    description: "Santa Claus getting his shop on",
-    price: 5000000
+    lat: 45.79, 
+    lng: 103.23,
+    name: "Newly Built Modern Home w/free parking!",
+    description: "Do you want to enjoy a stylish experience at this centrally-located home on an island city?  Do you want to have the convenience of walking to our mile long beach where you can watch the sunsets over the San Francisco?    Then come over and enjoy my newly built mid century modern cottage in the friendly island town of Alameda!",
+    price: 155
   },
   {
     ownerId: 2,
-    address: "178 Innout Burger Lane",
-    city: "Pasadena",
+    address: "259 Page Street",
+    city: "San Francisco",
     state: "CA",
     country: "USA",
-    lat: 895.230,
-    lng: 345.697,
-    name: "In-n-Out Burger",
-    description: "Making the best burgers and you know it",
-    price: 6700000
+    lat: 23.04,
+    lng: 143.79,
+    name: "Golden Gate Apex Hideaway",
+    description: "Wake up to rooftop views and sink into an inviting armchair under the eaves of this vibrant third-floor oasis. Freshen up in the chic, tiled shower with a revitalizing rain dance mode, then take it easy in front of a movie on the 72-inch TV.    The Orange Suite is a convenient, kitchenless studio on the 3rd floor of my house! It offers a comfy firm queen bed, a new bathroom and fixtures, refrigerator, toaster, microwave, espresso maker, as well as the sweetest sitting area in orange you ever will see with couch, chairs, bean bag chair and tv. I love spending time under the cozy slanted ceilings!",
+    price: 141
   },
   {
     ownerId: 3,
-    address: "4567 Athens Drive",
-    city: "Savannah",
-    state: "GA",
+    address: "4361 Lincoln Ave",
+    city: "Oakland",
+    state: "CA",
     country: "USA",
     lat: 69.504,
     lng: 34.509,
-    name: "Heaven",
-    description: "Heaven on earth",
-    price: 5940234
+    name: "Hiking, Biking & Bay Area Views!",
+    description: "This beautiful home in the east bay hills boasts spectacular views of the entire bay area, from San Jose to Marin. It is a short walk or drive from many miles of trails for hiking, horseback riding and mountain biking. It also provides immediate access to many miles of the bay area's best road biking on Skyline Boulevard. This tranquil retreat is a short drive to Montclair Village, Rockridge, Temescal, Piedmont and Berkeley for a wide variety of amazing dining, music and fun! Enjoy the view!",
+    price: 486
   },
   {
     ownerId: 4,
-    address: "456 Eightball Drive",
-    city: "Pooltable",
-    state: "OH",
+    address: "188 King Street",
+    city: "San Francisco",
+    state: "CA",
     country: "USA",
     lat: 39.30, 
     lng: 104.23,
-    name: "Hole in the wall Pool",
-    description: "It's a pool table",
-    price: 40
+    name: "Entire condo in San Francisco, California",
+    description: "This condo is located in the Heart of San Francisco is close in proximity to many amazing local attractions, restaurants, and transportation options. Best Waterfront & City View + Free Reserved Parking. A New Experience for Better Stay/Living - Retreat and boost for your work week and getaway! Live, work, and play in style.",
+    price: 680
   },
   {
     ownerId: 5,
-    address: "235 Taco Court",
-    city: "Irvine",
+    address: "1935 Webster Street",
+    city: "Oakland",
     state: "CA",
     country: "USA",
     lat: 50.43,
     lng: 13.76,
-    name: "Not a Taco Bell",
-    description: "Joking its a taco bell",
-    price: 125
+    name: "Kaval Park - Peaceful Urban Oasis",
+    description: "Kick back and relax in this serene modern home. Enjoy your morning coffee on the flowering garden patio, read a book and pick your own figs, lemons from the garden. It's a private, peaceful and beautiful space, designed with minimum impact in mind - on the border with Berkeley, Oakland and Emeryville, across Bay Bridge from San Francisco. Hop on the Bay Bridge and go into the city - San Francisco is a short drive away, 20 - 40 min depends on the traffic! Fiber Optic Wi-Fi. Off street parking.",
+    price: 325
   },
   {
     ownerId: 6,
-    address: "940 Bose Drive",
-    city: "Sleepbuds",
-    state: "IL",
+    address: "1001 Rispin Dr",
+    city: "Oakland",
+    state: "CA",
     country: "USA",
     lat: 32.09,
     lng: -4.39,
-    name: "Sleep Buds",
-    description: "Not working but had to send them back",
-    price: 97
+    name: "Famed Maybeck Mansion and Botanical Park",
+    description: "A 4500 sq ft. home designed by the architect of the Palace of Fine Arts, Bernard Maybeck in the midst of a landscaped botanical park. The residence is in a hidden enclave close to the University of California Berkeley and the Claremont Hotel. Past guests include movie stars, iconic chefs such as Julia Childs, Academy award winning music has been composed on the piano in the living room and we look forward to hosting you as well.",
+    price: 2500
   },
   {
     ownerId: 1,
-    address: "457 Xbox Drive",
-    city: "Belevue",
-    state: "WA",
+    address: "1003 61st Street",
+    city: "Oakland",
+    state: "CA",
     country: "USA",
     lat: 45.78, 
     lng: -57.90,
-    name: "Microsoft HQ",
-    description: "Making Green Boxes",
-    price: 234567
+    name: "Private room in guest suite in Oakland, California",
+    description: "This house is a multiplex. My husband and I live upstair unit 3. If you stay in this house, you will stay in a separate downstair unit 1, 2 & container in backyard. There is a door accessing the backyard from the last room of the main unit. You can go to the backyard, container, basement unit and the laundry room. Also you can access backyard through an external side wooden gate as well.",
+    price: 477
   },
   {
     ownerId: 2,
-    address: "178 Narobia Street",
-    city: "Savannah",
-    state: "GA",
+    address: "1725 North Point Street",
+    city: "San Francisco",
+    state: "CA",
     country: "USA",
     lat: 57.90,
     lng: 45.63,
-    name: "Narobia Dining",
-    description: "Best breakfast you've ever had",
-    price: 67
+    name: "•:• Marina Star SF •:• 1 BR Private Modern Suite",
+    description: "✸ Marina Star SF Suite ✸ A private, one-bedroom suite that is spacious (1,000 sf), bright, and centrally located. This beautifully-appointed flat is architect designed and newly remodeled with unique finishes and top-of-the-line amenities. Conveniently located in the heart of the Marina, sight seeing, shopping, cafes, and many of San Francisco's best restaurants are merely minutes from the front door.",
+    price: 225
   },
   {
     ownerId: 3,
-    address: "157 Google Pixel Drive",
-    city: "San Mateo",
+    address: "246 Mountain Home Road",
+    city: "Woodside",
     state: "CA",
     country: "USA",
     lat: 35.20,
     lng: 34.509,
-    name: "Google pixel phones",
-    description: "It's a phone!",
-    price: 5940234
+    name: "Luxury Mid-Century Modern Home w/ XL Deck",
+    description: "Newly remodeled 5-bedroom (note: 1 bedroom doubles as an office), 3-bath home - walking distance to the Mountain Terrace wedding venue, Alice's Restaurant, and 10 minutes from Sand Hill Road in Palo Alto. Can accommodate up to 12 guests to comfortably sleep.  NOTE: We do allow gatherings, events, and parties to be held on-site for a $1500 fee; please message us for approval.",
+    price: 953
   },
   {
     ownerId: 4,
-    address: "983 Cucapa Drive",
-    city: "Flagstaff",
-    state: "AZ",
+    address: "32 Ford Street",
+    city: "San Francisco",
+    state: "CA",
     country: "USA",
     lat: 45.32, 
     lng: -19.23,
-    name: "Beer Cap",
-    description: "Beer on the map",
-    price: 115
+    name: "Fabulous Studio in the Castro District",
+    description: "This classy studio is in the heart of Castro, close to restaurants, bars, coffee shops and public transportation. The studio has a king size Casper bed, large Smart TV (no cable), Sonos sound system, a kitchenette with a refrigerator, dishwasher (huge plus!), sink, microwave, and coffee maker (no stove). There is an in unit washer and dryer for your convenience.",
+    price: 296
   },
   {
     ownerId: 5,
-    address: "157 Dust Bowl Street",
-    city: "Turlock",
+    address: "6701 Shellmound Street",
+    city: "Emeryville",
     state: "CA",
     country: "USA",
     lat: 45.23,
     lng: 9.18,
-    name: "Dust Bowl Brewery",
-    description: "Serving great beer",
-    price: 95
+    name: "Cozy Emeryville Studio, Near Beaches & Parks!",
+    description: "Experience Emeryville with ease in this studio, 2-bathroom vacation rental. Offering a full kitchen, a comfortable living area with a sauna, Smart TVs, and access to the rooftop, this property is perfect for sharing memory-filled moments or enjoying a solo retreat! Centrally located, this property allows for beachside days or time spent exploring Jack London Square. Find your next perfect pairing at Cinque Capre Vineyards or York Creek Vineyards. 'Vacation You' is just around the corner!",
+    price: 321
   },
   {
     ownerId: 6,
-    address: "157 Cigar City",
-    city: "Miami",
-    state: "FL",
+    address: "1802 Bonita Ave",
+    city: "Berkeley",
+    state: "CA",
     country: "USA",
     lat: 9.45,
     lng: -42.39,
-    name: "Cigar City Brewery",
-    description: "Hip Hip, Huray!",
-    price: 170
+    name: "(3 BED)Modern Two BED + Detached Studio in Elmwood",
+    description: "This unique 3 Bed rental is a combo of a 2 Bed garden level of a newly remodeled duplex as well as a detached studio in the heart of Elmwood located walking distance to the University, shops, and BART. You will have access to both the garden suite and the studio. Garden Suite has 2 Bedrooms, a kitchenette and living room with minimalist style. Studio is separate and detached. The garden suite is connected via a stair case to the owners level above but you will have private entrance and access.",
+    price: 500
   },
 ]; 
 
@@ -172,7 +172,7 @@ module.exports = {
     options.tableName = 'Spots';
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(options, {
-      address: { [Op.in]: ["123 Jinglebell Lane", "178 Innout Burger Lane", "4567 Athens Drive"] }
+      country: { [Op.in]: ["USA"] }
     }, {});
   },
 };
--- backend/db/seeders/20231223000354-SpotImageSeeder.js
+++ backend/db/seeders/20231223000354-SpotImageSeeder.js
@@ -9,49 +9,184 @@ if (process.env.NODE_ENV === 'production') {
 const validSpotImages = [
   {
     spotId: 1,
-    url: "www.randomimage.com/1",
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-770631037940585870/original/0ecd137d-d045-40d9-9f03-1fe62d166338.jpeg?im_w=1200",
     preview: true,
   },
   {
     spotId: 1,
-    url: "www.randomimage.com/2",
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-770631037940585870/original/4cd5b58b-4b44-4dc0-ab6d-df9559578790.jpeg?im_w=1440",
     preview: true,
   },
   {
     spotId: 1,
-    url: "www.randomimage.com/3",
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-770631037940585870/original/e4504573-faf5-4b4d-834a-83fb68c45c97.jpeg?im_w=1440",
     preview: true,
   },
   {
     spotId: 2,
-    url: "www.randomimage.com/4",
+    url: "https://a0.muscache.com/im/pictures/d9f7d265-cbca-47a7-91e9-6293692789a6.jpg?im_w=1200",
     preview: true,
   },
   {
     spotId: 2,
-    url: "www.randomimage.com/5",
+    url: "https://a0.muscache.com/im/pictures/3c97ee26-580c-4bc8-b8e8-d19f1c8c23b4.jpg?im_w=1440",
     preview: true,
   },
   {
     spotId: 2,
-    url: "www.randomimage.com/6",
+    url: "https://a0.muscache.com/im/pictures/5db7fe79-9758-464b-ade3-a0aeaa3e2382.jpg?im_w=1440",
     preview: true,
   },
   {
     spotId: 3,
-    url: "www.randomimage.com/7",
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-904459261671463001/original/9e78dfce-eb3e-45a2-a66d-bd9e6414dee0.jpeg?im_w=1440",
     preview: true,
   },
   {
     spotId: 3,
-    url: "www.randomimage.com/8",
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-904459261671463001/original/e0595c82-6a36-4ee9-973b-5ab06522dbba.jpeg?im_w=1200",
     preview: true,
   },
   {
     spotId: 3,
-    url: "www.randomimage.com/9",
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-904459261671463001/original/5160a206-5c10-4007-8726-7fe655aab784.jpeg?im_w=1440",
     preview: true,
-  }
+  },
+  {
+    spotId: 4,
+    url: "https://a0.muscache.com/im/pictures/hosting/Hosting-975388631678738792/original/84ceb98f-f063-4694-bfde-22c102505eb1.jpeg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 4,
+    url: "https://a0.muscache.com/im/pictures/hosting/Hosting-975388631678738792/original/467e0a86-81fb-4328-9629-b0c8bb31930c.jpeg?im_w=1440", 
+    preview: true,
+  },
+  {
+    spotId: 4,
+    url: 'https://a0.muscache.com/im/pictures/hosting/Hosting-975388631678738792/original/7e65da6b-1778-4593-bb8e-d0df1ba13086.jpeg?im_w=1440',
+    preview: true,
+  },
+  {
+    spotId: 5,
+    url: "https://a0.muscache.com/im/pictures/3a963607-ab65-4089-95ab-30829b46217f.jpg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 5,
+    url: "https://a0.muscache.com/im/pictures/97fd3590-99c2-4bb2-93b1-4f0934a41262.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 5,
+    url: "https://a0.muscache.com/im/pictures/adc746b7-551d-47de-a5b9-e4cd7e178efb.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 6,
+    url: "https://a0.muscache.com/im/pictures/6003384/4dfc82ed_original.jpg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 6,
+    url: "https://a0.muscache.com/im/pictures/8818242/f76871a6_original.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 6,
+    url: "https://a0.muscache.com/im/pictures/6003416/98bd9e4c_original.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 7,
+    url: "https://a0.muscache.com/im/pictures/f1c9339b-8851-4719-ba3e-cb7a5c87fdad.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 7,
+    url: "https://a0.muscache.com/im/pictures/60c744c3-0629-43c3-b4d1-22739cb7ad1c.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 7,
+    url: "https://a0.muscache.com/im/pictures/338af0ff-bc0b-4c38-8025-3efec79a150e.jpg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 8,
+    url: "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTA2ODIwMzU2NjM5MTE3NzY0OQ%3D%3D/original/6a2f98bf-c704-4a97-80ef-673f4861bd14.jpeg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 8,
+    url: "https://a0.muscache.com/im/pictures/hosting/Hosting-U3RheVN1cHBseUxpc3Rpbmc6MTA2ODIwMzU2NjM5MTE3NzY0OQ%3D%3D/original/714ad01d-3d37-4cbc-b473-154413f758c8.jpeg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 8,
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-1068203566391177649/original/19d1ca1c-d8a5-4c2e-a173-c8040dc08a21.jpeg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 9,
+    url: "https://a0.muscache.com/im/pictures/airflow/Hosting-869136186140044480/original/dfdf410c-3d2c-49f1-bf35-e229fdc4e4f8.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 9,
+    url: "https://a0.muscache.com/im/pictures/airflow/Hosting-869136186140044480/original/21a4a817-5138-4198-9a67-a13a0d710a8b.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 9,
+    url: "https://a0.muscache.com/im/pictures/airflow/Hosting-869136186140044480/original/f628e78a-2e50-4d51-a14e-7898f81c5e7d.jpg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 10,
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-663047756982570616/original/6900d208-ba73-452e-ac9b-d7af68f08961.jpeg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 10,
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-663047756982570616/original/016c9eaf-d804-4edf-b4e3-b101c4c2e3c7.jpeg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 10,
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-663047756982570616/original/0c7d56a5-5f52-4b0d-af2c-b29ecbdcf46b.jpeg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 11,
+    url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-736124896461793076/original/4adec904-15aa-4831-9e01-e2b82f3e40a5.jpeg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 11,
+    url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-736124896461793076/original/c430bb61-43eb-4a47-9885-0449e9b33182.jpeg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 11,
+    url: "https://a0.muscache.com/im/pictures/prohost-api/Hosting-736124896461793076/original/027611b9-1e97-4d2f-9621-0150de5e3a85.jpeg?im_w=1440",
+    preview: true,
+  },
+  {
+    spotId: 12,
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-45676925/original/89fe564a-53cf-4580-bbba-6041b9a6675d.jpeg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 12,
+    url: "https://a0.muscache.com/im/pictures/miso/Hosting-45676925/original/93bc709f-cd5e-4652-8904-0aea1b65ffff.jpeg?im_w=1200",
+    preview: true,
+  },
+  {
+    spotId: 12,
+    url: "https://a0.muscache.com/im/pictures/9177e746-feda-4119-a1cd-34dee6e00cda.jpg?im_w=1200",
+    preview: true,
+  },
 ]
 
 /** @type {import('sequelize-cli').Migration} */
